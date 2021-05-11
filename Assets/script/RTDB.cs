fd3b6df693b915360cb141d0a5aa4ca6df91cd22
zltm <jdavidrevillad@gmail.com>
2021-05-11 18:05:31 -0400

    befor firebase asset

--- Assets/script/RTDB.cs
+++ Assets/script/RTDB.cs
@@ -8,14 +8,36 @@ public class RTDB : MonoBehaviour
     [SerializeField] TMP_InputField username;
     [SerializeField] TMP_InputField email;
     [SerializeField] TMP_InputField nametoread;
-    [SerializeField] Text data;
+    [SerializeField] TextMeshProUGUI data;
+    string PulledName;
+    string UpdatedName;
     // Start is called before the first frame update
 
     User user = new User();
+    void Start() 
+    {
+        UpdatedName = "";
+    }
+
+    void Update() 
+    {
+        data.text = UpdatedName;
+    }
     public void savedata()
     {
         user.UserName = username.text;
         user.Email = email.text;
+        user.Ichi = "0";
+        user.Ni = "0";
+        user.San = "0";
+        user.Yon = "0";
+        user.Go = "0";
+        user.Roku = "0";
+        user.Nana = "0";
+        user.Hachi = "0";
+        user.Juu = "0";
+        user.Hi = "0";
+
         string json = JsonUtility.ToJson(user);
 
         FirebaseDatabase.DefaultInstance.RootReference.Child("User").Child(user.UserName).SetRawJsonValueAsync(json).ContinueWith(task =>
@@ -39,16 +61,20 @@ public class RTDB : MonoBehaviour
             {
                 Debug.Log("successfull");
                 DataSnapshot snapshot = task.Result;
-                print("username is");
-                Debug.Log( snapshot.Child("UserName").Value.ToString());
-                print("email is");
-                Debug.Log(snapshot.Child("Email").Value.ToString());
-
+                PulledName = snapshot.Child("UserName").Value.ToString();
+                Debug.Log("user: "+snapshot.Child("UserName").Value.ToString());
+                Debug.Log("email: "+snapshot.Child("Email").Value.ToString());
             }
             else
             {
                 Debug.Log("not successfull");
             }
+            SetText(PulledName);
         });
+    }
+
+    public void SetText(string UsedName)
+    {
+        UpdatedName = UsedName;
     }
 }
--- Assets/script/User.cs
+++ Assets/script/User.cs
@@ -6,4 +6,14 @@ public class User
 {
     public string UserName;
     public string Email;
+    public string Ichi;
+    public string Ni;
+    public string San;
+    public string Yon;
+    public string Go;
+    public string Roku;
+    public string Nana;
+    public string Hachi;
+    public string Juu;
+    public string Hi;
 }
